c9ce4eb2e2b017ddd96378ee1da5ac09cc0635de
ArmaniL <35434507+ArmaniL@users.noreply.github.com>
2020-07-28 01:03:00 -0400

    Update Sketch.js
    implementrdd it without p5

--- Sketch.js
+++ Sketch.js
@@ -1,73 +1,60 @@
-var A
-var B
-var C
-var sol1=0
-var sol2=0
-var bt
-var discr;
-var a
-var b
- var c
+const input=document.getElementsByClassName("equation")[0]
+const display=document.getElementsByClassName("results")[0]
 
-function setup(){
-createCanvas(500,500);
-frameRate(30);
-background(0)
-A=createInput();
-A.position(90,140)
-A.value('1')
-A.size(25,22)
-B=createInput();
-B.position(210,140)
-B.value('1')
-B.size(25,22)
-C=createInput();
-C.position(330,140)
-C.value('1')
-C.size(25,22)
-bt=createButton("Solve")
-bt.position(225,250)
-bt.mousePressed(Solve)
+
+input.addEventListener("input",update)
+
+function parse(equation){
+let b=0
+let c=0
+let a=0
+const  re=/(-?\d+)x\^2(([+-]\d+)x)?([+-]\d+)?/
+const coefficients=equation.trim().match(re)
+console.log(coefficients)
+ a=parseInt(coefficients[1])
+if(coefficients[3]){
+ b=parseInt(coefficients[3])
 }
 
-function draw(){
-background(10)
-stroke(47, 120*Math.cos(16*Math.PI*frameCount)+120, 38)
-fill(47,120*Math.sin(16*Math.PI*frameCount)+120,38)
-textFont('Helvetica')
-textSize(30)
-text("QuadSolver",width/3,33)
-textSize(20)
-text("x",114,150)
-textSize(10)
-text("2",126,135)
-textSize(20)
-text("+",160,150)
-text("x",234,150)
-text("+",280,150)
-text(sol1,100,400)
-text(sol2,400,400)
- a=parseFloat(A.value()) 
- b=parseFloat(B.value())
- c=parseFloat(C.value())
+if(coefficients[4]){
+ c=parseInt(coefficients[4])
+}
+
+const g =solve(a,b,c)
+console.log(g)
+return g 
+
+
+}
+
+function solve(a,b,c){
+const discrim=(Math.pow(b, 2) - (4 * a * c))
+if(discrim>0){
+const f= ((-1 * b + Math.sqrt(discrim) / (2 * a))).toFixed(2)
+const s=((-1 * b - Math.sqrt(discrim) / (2 * a))).toFixed(2)
+return{
+
+    first:f,
+    second:s
+
+}
+}
+else if (discrim<0){
+    return{ message:'Answer Not real'}
+}
+else{  
+     const f=((-1 * b + Math.sqrt(discrim) / (2 * a))).toFixed(2)
+     return {first:f }    }    
+
 
 }
 
 
-function Solve(){
-
- discr=Math.pow(b,2)-4*a*c
-
- if(discr==0){sol1=  (Math.sqrt(discr)-b)/(2*a)
-sol2=sol1
- }
-else if (discr>0){
-sol1= Math.round(10000*(Math.sqrt(discr)-b)/(2*a))/10000
-sol2=Math.round(10000*(-1*Math.sqrt(discr)-b)/(2*a))/10000
-}
+function update(e){
+const answers = parse(e.target.value)
+if(answers.message){  display.textContent=answers.message   }
 else{
-sol1="Not"
-sol2="Real"
+const result=    answers.second===undefined?    "             "+answers.first  :"        "+answers.first+"            "+answers.second+""
+display.textContent=result
 }
-
 }
